e87f9768af02d527a2eab8246fd018b48f46fc1d
CarlosLucioDmz <102681952+carloselucio@users.noreply.github.com>
2023-03-10 10:31:15 -0600

    Change Structure

--- controllers/test.js
+++
@@ -1,8 +0,0 @@
-exports.test_get = async function(req, res) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    res.status(200).json({
-        test: 'hello world'
-    });
-}
--- routes/test.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let testController = require('../controllers/test');
-require("dotenv").config()
-
-/* GET home page. */
-router.get(process.env.API_URL + process.env.TEST_URL , testController.test_get);
-
-module.exports = router;
